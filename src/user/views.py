5367bab91736749237dd12d6e927bab9d25c8813
bizunok <doxamv@gmai.com>
2025-09-09 23:11:34 +0300

    urls added register2

--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.views.generic.edit import CreateView
+from .forms import UserRegisterForm
+from django.urls import reverse_lazy
 
 class LoginUser(LoginView):
     template_name = "login.html"
@@ -7,3 +11,10 @@ class LoginUser(LoginView):
 
 class LogoutUser(LogoutView):
     template_name = None
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+    form_class = UserRegisterForm
+    success_message = "Your profile was created successfully"
